9705d0843bac12f7c7dc83841f14bb7a0723a204
Cesar Enrique Beltran Feliz <cesar78200921@hotmail.com>
2023-08-31 16:06:25 -0400

    Archivos iniciales

---
+++ vue3-las-bases/Vue/composition-app/src/counter/views/CounterView.vue
@@ -0,0 +1,30 @@
+﻿<script lang="ts" src="src/counter/views/CounterView.ts">
+
+// import {defineComponent, ref} from "vue";
+//
+// const counter = ref(5);
+//const increase = () =>{counter.value++}
+
+// export default defineComponent({
+//   setup()
+//   {
+//    
+//     const counter = ref(5)
+//    
+//     const increase = () => {
+//       counter.value++;
+//     }
+//    
+//     return{
+//       counter,
+//       increase,
+//     }
+//   }
+// });
+</script>
+
+
+<template>
+  <h1>Counter View: {{counter}}</h1>
+  <button @click="counter++">+1</button>
+</template>
