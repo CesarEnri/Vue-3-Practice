3c2eb142b547d09a057193e5c80e9a8417e40f21
Enrique Beltran <cesar78200921@hotmail.com>
2023-09-03 19:18:28 -0400

    Card of pokemons

--- vue3-las-bases/Vue/composition-app/src/pokemons/pages/PokemonList.vue
+++ vue3-las-bases/Vue/composition-app/src/pokemons/pages/PokemonList.vue
@@ -1,19 +1,27 @@
 ﻿<script setup lang="ts">
 import {usePokemons} from "@/pokemons/composables/usePokemons";
+import PokemonCardList from "@/pokemons/components/PokemonCardList.vue";
 
-const {count, pokemons, isLoading} = usePokemons();
+const {count, pokemons, isLoading, isError, error} = usePokemons();
 
 </script>
 
 <template>
-  <h1>Pokemon List - {{ count }}</h1>
+  <h1 v-if="isLoading">Loading</h1>
   
-  <h3 v-if="isLoading">Cargando</h3>
-  <ul>
-    <li v-for="pokemon in pokemons" :key="pokemon.id">
-      {{pokemon.name}}
-    </li>
-  </ul>
+  <div v-else-if="error">
+    {{error}}
+  </div>
+  
+  <div v-else>
+    
+    <h1>Pokemon List - {{ count }}</h1>
+
+  <PokemonCardList :pokemons="pokemons ?? []"/>
+    
+  </div>
+  
+  
   
 </template>
 
